8cf4947af6542ae402abbd54ec248adc7b8e1b02
chinmaya <chinmaya@gmail.com>
2025-05-08 18:17:48 +0530

    setup eks cluster

--- 1.providers.tf
+++ 1.providers.tf
@@ -12,8 +12,28 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.36.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.0-pre2"
+    }
   }
 }
+
+provider "kubernetes" {
+  host = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command = "aws"
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
 
 
 
---
+++ 3.main-eks.tf
@@ -0,0 +1,89 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# VPC for Cluster
+data "aws_availability_zones" "azs" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.21.0"
+
+  name = var.name
+  cidr = var.vpc_cidr_block
+
+  azs = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnets_cidr_blocks
+  public_subnets  = var.public_sunets_cidr_blocks
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = var.tags
+}
+
+# EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.31"
+
+  cluster_name = var.name
+  cluster_version = var.k8s_version
+  cluster_endpoint_public_access = true
+
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_addons = {
+    kube-proxy = {}
+    vpc-cni = {}
+    coredns = {}
+  }
+
+  eks_managed_node_groups = {
+    initial = {
+        instance_type = ["t2.medium"]
+        min_size = 2
+        max_size = 4
+        desired_size = 2
+    }
+  }
+
+  tags = var.tags
+}
+
+module "eks-blueprints-addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.0"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  enable_metrics_server = true
+  cluster_autoscaler = {
+    set = [
+      {
+        name = "extraArgs.scale-down-unneeded-time"
+        value = "1m"
+      },
+      {
+        name = "extraArgs.skip-nodes-with-local-storage"
+        value = false
+      },
+      {
+        name = "extraArgs.skip-nodes-with-system-pods"
+        value = false
+      }
+    ]
+  }
+}
