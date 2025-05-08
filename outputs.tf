e85c2ba16a62d3673e18f7e28efb613192359277
chinmaya <chinmaya@gmail.com>
2025-05-08 18:26:21 +0530

    create outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,26 @@
-output "alb_dns_name" {
-  description = "DNS of the application load balancer"
-  value       = aws_lb.web_lb.dns_name
+# CLuster Info
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_platform_version" {
+  description = "Platform version for the cluster"
+  value = module.eks.cluster_platform_version
+}
+
+output "cluster_status" {
+  description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
+  value = module.eks.cluster_status
+}
+
+# Kubectl Configuration
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.aws_region}"
 }
