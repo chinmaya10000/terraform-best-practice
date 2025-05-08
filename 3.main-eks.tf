1084178937c986b8faf35e1e8121c52ee1651485
chinmaya <chinmaya@gmail.com>
2025-05-08 18:49:13 +0530

    fix issue

--- 3.main-eks.tf
+++ 3.main-eks.tf
@@ -3,17 +3,18 @@ provider "aws" {
 }
 
 provider "helm" {
-  kubernetes {
-    host = module.eks.cluster_endpoint
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
-      command = "aws"
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
+
 
 # VPC for Cluster
 data "aws_availability_zones" "azs" {}
